69d347dea6a836ff7f5ffedbcde997564464927b
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-18 15:49:58 -0400

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -848,6 +848,66 @@ const pmExtensions = [
     },
 ];
 
+// In below array, there is some extensions that were taken from GaiaMod
+const gmExtensions = [
+        {
+        name: "Gaia Utilities",
+        description: "Wonderful utility blocks!",
+        code: "GaiaWindWave90/GaiaBlocks.js",
+        banner: "GaiaWindWave90/GaiaUtilities.png",
+        creator: "GaiaWindWave90 and other users",
+        isGitHub: true,        
+    },
+    {
+        name: 'GaiaGPT',
+        description: 'A GaiaMod modification of Pang AI with more prompts.',
+        code: "GaiaWindWave90/GaiaGPT.js",
+        banner: "GaiaWindWave90/GaiaGPT.png",
+        creator: "logise1123",
+        isGitHub: true,
+    },
+    {
+        name: 'Prompts',
+        description: 'Prompts made for Gaia AI. Inspired by the prompts extension found in Snail-IDE.',
+        code: "GaiaWindWave90/GTPPrompts.js",
+        banner: "GaiaWindWave90/prompts.png",
+        creator: "GaiaWindWave90",
+        isGitHub: true,
+    },
+    {
+        name: 'Spinach Facts',
+        description: 'An extension about spinach! Based on Gen1x\'s CATS, MrIncredibleMaker\'s DOGS and pooiod7\'s Raccoon Facts extensions.',
+        code: "GaiaWindWave90/SpinachFacts.js",
+        banner: "GaiaWindWave90/spinach.png",
+        creator: "GaiaWindWave90",
+        isGitHub: true,
+    },
+    {
+        name: "Penguin Attack!",
+        description: "Edited version of the Fire in the Hole extension.",
+        code: "GaiaWindWave90/PenguinAttack.js",
+        banner: "GaiaWindWave90/PenguinAttack.png",
+        creator: "GaiaWindWave90",
+        isGitHub: true,        
+    },
+    {
+        name: "Cocrea Fetch",
+        description: "Fetches API from Cocrea or Gandi IDE.",
+        code: "GaiaWindWave90/cocreaFetch.js",
+        banner: "GaiaWindWave90/cocreaFetch.png",
+        creator: "fath11",
+        isGitHub: true,        
+    },
+    {
+        name: "Webcam Recorder",
+        description: "Records videos straight out of webcam.",
+        code: "GaiaWindWave90/Webcamrecorder.js",
+        banner: "GaiaWindWave90/WebCamThumb.png",
+        creator: "-SIPC-",
+        isGitHub: true,        
+    },
+];
+
 // In below array, there is some extensions that were taken from RubyDevs
 const otherExtensions = [
         {
@@ -860,4 +920,4 @@ const otherExtensions = [
     },
 ];
 
-export {extensions, pmExtensions, otherExtensions}
+export {extensions, pmExtensions, gmExtensions, otherExtensions}
